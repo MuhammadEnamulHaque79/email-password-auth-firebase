711efc398f501e84a4f8d5475b38ea39f69a930c
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-05-01 02:26:42 +0600

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProviders';
 
 const Header = () => {
+    const { user,logOut} = useContext(AuthContext);
+    const handleLogOut=()=>{
+        logOut()
+            .then(()=>{
+                console.log('Sign-out successful.')
+            })
+            .catch(()=>{
+                console.log('An error happened.')
+            })
+    }
+
     return (
-       
-            <div className="navbar bg-neutral text-neutral-content">
-                <div className='right-nav-link'>
+
+        <div className="navbar bg-neutral text-neutral-content">
+            <div className='right-nav-link'>
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>Hey Man</Link>
-                </div>
-                <div className='left-nav-link'>
-                    <Link className="btn btn-ghost normal-case text-xl font-thin from-neutral-focus font-mono" to='/'>Home</Link>
-                    <Link className="btn btn-ghost normal-case text-xl font-thin from-neutral-focus font-mono" to='/login'>Login</Link>
-                    <Link className="btn btn-ghost normal-case text-xl font-thin from-neutral-focus font-mono" to='/register'>Register</Link>
-                </div>
+                {
+                    user ?
+                        <span>{user.email}
+                            <button onClick={handleLogOut} className="btn btn-sm">Sign Out</button>
+                        </span>
+                        :
+                        <span>
+                            <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
+                        </span>
+                }
             </div>
-       
+
+            <div className='left-nav-link'>
+                <Link className="btn btn-ghost normal-case text-xl font-thin from-neutral-focus font-mono" to='/'>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl font-thin from-neutral-focus font-mono" to='/login'>Login</Link>
+                <Link className="btn btn-ghost normal-case text-xl font-thin from-neutral-focus font-mono" to='/register'>Register</Link>
+            </div>
+        </div>
+
     );
 };
 
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState} from 'react';
+import React, { createContext, useEffect, useState} from 'react';
 import app from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 
 const auth = getAuth(app);
@@ -18,10 +18,26 @@ const signIn =(email,password)=>{
     return signInWithEmailAndPassword(auth,email,password);
 }
 
+const logOut=()=>{
+   return signOut(auth);
+}
+
+useEffect(()=>{
+   const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+        setUser(currentUser);
+    })
+    return()=>{
+        unsubscribe();  
+    } 
+},[]);
+
+
 const userInfo={
     user,
     createUser,
-    signIn
+    signIn,
+    logOut,
+    setUser
 }
 
     return (
